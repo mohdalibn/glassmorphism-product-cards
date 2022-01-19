182215b5c18aa047f12b8792d8e892a38854285a
Mohd Ali Bin Naser <mohdalibin2001@gmail.com>
2022-01-19 23:27:45 +0300

    Github issue-1 progress checkpoint-1

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 
 // Script to swap card images on click
 
-function ImageSwap(imageSrc){
+// function ImageSwap(imageSrc){
     
-    document.querySelector('.main-img').src = imageSrc;
+//     document.querySelector('.main-img').src = imageSrc;
 
-}
+// }
+
+
+const ColorSelections = document.querySelectorAll('.color-circle');
+
+ColorSelections.forEach((colorOption, i) =>{
+    colorOption.addEventListener('click', () =>{
+        color
+    })
+})
